3f3ca4180e1c60f7b777de74af2e9df058e7532d
Olga Gergel <pyfok2000@gmail.com>
2020-06-01 16:42:42 +0200

    moj pierwszy commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pierwiastek
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+
+        {
+            double a, pierwiastek;
+            try
+            {
+                Console.WriteLine("Ten program oblicza pierwiastek z podanej przez uzytownika liczbe");
+                Console.WriteLine("Podaj liczbe a");
+                a = int.Parse(Console.ReadLine());
+                pierwiastek = Math.Sqrt(a);
+                Console.WriteLine("pierwiastek = {0:#.##}", Math.Sqrt(a));
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Nie wczytano liczby");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
